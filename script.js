b649d49c9e0b98ae8c817120d90cfe15913ec1f4
lcalapc <74427491+HiddenBeater@users.noreply.github.com>
2024-07-15 22:29:11 -0700

    Create computerchoice function in JS

---
+++ script.js
@@ -0,0 +1,13 @@
+// Create function that generates computer choice
+// of rock, paper, or scissors
+function getComputerChoice (){
+ // Randomly generate number between 0 to 2
+       let x = (Math.random() * 4)
+       console.log(x)
+       
+}
+
+getComputerChoice();
+
+// Appoint generated random number to assigned choice
+// Display assigned choice in string
