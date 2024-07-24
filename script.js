5bb43c23cc7c54cf828e91044ebf93839e59a3ce
lcalapc <74427491+HiddenBeater@users.noreply.github.com>
2024-07-23 17:21:03 -0700

    Readded messages on human and computer answers to show choices.

--- script.js
+++ script.js
@@ -6,13 +6,16 @@ function getComputerChoice (){
 // Appoint generated random number to assigned choice
        if (x === 0){
             let compChoice = ('rock');
+            console.log ('Computer chose rock.');
             return compChoice;
 // Display assigned choice in string
        } else if (x === 1){
             let compChoice = ('paper');
+            console.log('Computer chose paper.');
             return compChoice;
        } else if (x === 2){
             let compChoice = ('scissors');
+            console.log('Computer chose scissors.')
             return compChoice;
        }
 }
@@ -23,39 +26,34 @@ function getHumanChoice(){
 // Convert string to uppercase first letter
       humanChoice.toLowerCase();
       let answer = humanChoice.toLowerCase();
+      console.log(`You chose ${answer}. `)
       return answer;
 }
 //Receive computer answer and human answer to play a game
 function playRound(computerChoice, humanChoice){
 // Declare human winner based on computer answer. Add score to human.
       if (computerChoice == 'rock' && humanChoice == 'paper'){
-            console.log('You win! Computed picked rock.')
             humanScore++;
             return humanScore;
       } else if (computerChoice == 'paper' && humanChoice == 'scissors'){
-            console.log('You win! Computer picked paper.')
             humanScore++;
             return humanScore;
       } else if (computerChoice == 'scissors' && humanChoice == 'rock'){
-            console.log('You win! Computer picked scissors.')
             humanScore++;
             return humanScore;
 // Declare computer winner based on human answer. Add score to computer
       } else if (computerChoice == 'rock' && humanChoice == 'scissors'){
-            console.log('Computer picked rock.')
             computerScore++;
             return computerScore;
       } else if (computerChoice == 'paper' && humanChoice == 'rock'){
-            console.log('Computer picked paper.')
             computerScore++;
             return computerScore;
       } else if (computerChoice == 'scissors' && humanChoice == 'paper'){
-            console.log('Computer picked scissors.')
             computerScore++;
             return computerScore;
       } else {
 // Declare no winner. No score added.
-            console.log("It's a draw. Nobody wins.");
+            console.log();
       }
 }
 // Start game
@@ -65,14 +63,17 @@ function playGame(){
       while (round < 6){
              // Round starts. Score gets added to human or computer.
             console.log(`Round ${round}`);
-            playRound(getComputerChoice(), getHumanChoice());
+            playRound(getComputerChoice(), getHumanChoice());  
+            console.log('Computer score: ' + computerScore); 
             console.log('Human score: ' + humanScore);
-            console.log('Computer score: ' + computerScore);   
             round++;
       }
       // Stop game at 5th round.
-      // Display final score for human
+      console.log('The final scores are: ' )
+      // Display final score for human.
+      console.log('Human score: ' + humanScore);
       // Display final score for computer.
+      console.log('Computer score: ' + computerScore); 
           
 }
 
